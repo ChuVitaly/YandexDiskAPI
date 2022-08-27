5fa46c7265da6996393bd800e8aaf419ce47f03c
Виталий Чухляев <vitaliy69@mail.ru>
2022-08-27 13:49:07 +0300

    I did the job. Great!!!

--- main.py
+++ main.py
@@ -2,22 +2,11 @@ from pprint import pprint
 
 import requests
 
-token = "y0_AQAAAAAASRvfAADLWwAAAADLFaZrcj2WwkIaQ2qcF2c3urTXV10ViL8"
-# url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-# headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
-url = "/home/vitaly/Документы/Нетология/HomeWorks/Hello.txt"
-
-
-# f = open("/home/vitaly/Документы/Нетология/HomeWorks/Hello.txt", "r", encoding='UTF-8')
-# print(f.read())
-# f.close()
-
-# r = requests.get(url_query, headers=headers)
-# print(r)
+# filename = '/home/vitaly/Изображения/2face.jpg'
 
 
 class YaUploader:
-    def __init__(self, token: str):
+    def __init__(self, token):
         self.token = token
 
     def headers(self):
@@ -27,7 +16,6 @@ class YaUploader:
         }
 
     def upload_link(self, disk_file_path):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
         url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.headers()
         params = {"path": disk_file_path, "overwrite": "true"}
@@ -35,13 +23,14 @@ class YaUploader:
         pprint(resp.json())
         return resp.json()
 
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def upload_file(self, disk_file_path, filename):
+        href = self.upload_link(disk_file_path=disk_file_path).get("href", "")
+        resp = requests.put(href, data=open(filename, 'rb'))
+        resp.raise_for_status()
+        if resp.raise_for_status() == None:
+            print("File uploaded to disk")
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    yandex_file = YaUploader(token=token)
+    yandex_file.upload_file("2face.jpg", filename)
