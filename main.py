394fd20a8b91fc518b7eb822b7c2e7e2fe941cd6
Виталий Чухляев <vitaliy69@mail.ru>
2022-08-27 12:06:45 +0300

    Added methods: headers,upload_link

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+from pprint import pprint
+
 import requests
 
 token = "y0_AQAAAAAASRvfAADLWwAAAADLFaZrcj2WwkIaQ2qcF2c3urTXV10ViL8"
+# url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+# headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token}'}
+url = "/home/vitaly/Документы/Нетология/HomeWorks/Hello.txt"
+
+
+# f = open("/home/vitaly/Документы/Нетология/HomeWorks/Hello.txt", "r", encoding='UTF-8')
+# print(f.read())
+# f.close()
+
+# r = requests.get(url_query, headers=headers)
+# print(r)
 
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
+    def headers(self):
+        return {
+            'Content-Type': 'application/json', 'Accept': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def upload_link(self, disk_file_path):
         """Метод загружает файлы по списку file_list на яндекс диск"""
+        url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        resp = requests.get(url_upload, headers=headers, params=params)
+        pprint(resp.json())
+        return resp.json()
+
         # Тут ваша логика
         # Функция может ничего не возвращать
 
